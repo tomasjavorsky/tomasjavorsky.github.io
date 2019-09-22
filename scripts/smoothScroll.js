519e783295f2a4f9b7b2191141929eee59e90906
Tomáš Javorský <tomasjavorsky@users.noreply.github.com>
2019-09-22 16:53:01 +0200

    smoothScroll

---
+++ scripts/smoothScroll.js
@@ -0,0 +1,15 @@
+// Smooth Scrolling
+$("#navbarNav a").on('click', function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function () {
+
+      window.location.hash = hash;
+    });
+  }
+});
